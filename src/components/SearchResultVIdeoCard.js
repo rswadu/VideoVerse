025a6de24a70c036f9117e2d53621ca4797feb12
rswadu <rswadu@gmail.com>
2023-06-25 12:21:52 +0530

    search result video card bg changed to blue

--- src/components/SearchResultVIdeoCard.js
+++ src/components/SearchResultVIdeoCard.js
@@ -1,20 +1,18 @@
-function SearchResultVIdeoCard({info}) {
+function SearchResultVIdeoCard({ info }) {
+  //     const {thubnails,title,channelTitle}=snippet;
+  //     const thumbnails=info?.snippet?.thumbnails;
+  // const title=info?.snippet?.title;
+  // const channelTitle=info?.snippet?.channelTitle;
 
-//     const {thubnails,title,channelTitle}=snippet;
-//     const thumbnails=info?.snippet?.thumbnails;
-// const title=info?.snippet?.title;
-// const channelTitle=info?.snippet?.channelTitle;
-
-const thumbnails=info?.snippet?.thumbnails;
-const title=info?.snippet?.title;
-const channelTitle=info?.snippet?.channelTitle;
+  const thumbnails = info?.snippet?.thumbnails;
+  const title = info?.snippet?.title;
+  const channelTitle = info?.snippet?.channelTitle;
   return (
-    <div className="m-2 p-2 w-80 bg-gray-300 hover:bg-gray-200 hover:shadow-2xl" >
-    <img src={thumbnails?.medium?.url} alt="" /> 
-    <p>{title}</p>
-    <p>{channelTitle}</p>
-    
+    <div className="m-2 p-2 w-80 bg-blue-300 hover:bg-blue-200 hover:shadow-2xl">
+      <img src={thumbnails?.medium?.url} alt="" />
+      <p>{title}</p>
+      <p>{channelTitle}</p>
     </div>
-  )
+  );
 }
-export default SearchResultVIdeoCard
+export default SearchResultVIdeoCard;
